b8ce4f5d6ca3d441320355d845f3c76d5ffeba4a
Ravindu Aluvihare <ravindu.aluvihare@outlook.com>
2025-03-27 18:04:25 +0530

    chatbot

---
+++ app.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get API key from environment variables
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize OpenAI client
+client = OpenAI(api_key=api_key)
+
+# API request
+response = client.chat.completions.create(
+    model="GPT-3.5-turbo",
+    messages=[{"role": "user", "content": "hello"}],
+    max_tokens=50,
+    temperature=0
+)
+
+# Print response
+for choice in response.choices:
+   print(choice.message.content)
